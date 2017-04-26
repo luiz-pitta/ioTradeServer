388570841f0864bb7b7ac3c98fc888f222e18af6
Luiz Guilherme Pitta <empreendedores.2016@gmail.com>
2017-04-26 20:47:43 -0300

    t

--- functions/services.js
+++ functions/services.js
@@ -3,21 +3,22 @@
 const db = require('../models/Connection');
 
 function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
-  var R = 6371; // Radius of the earth in km
-  var dLat = deg2rad(lat2-lat1);  // deg2rad below
-  var dLon = deg2rad(lon2-lon1); 
-  var a = 
+  const R = 6371; // Radius of the earth in km
+  const dLat = deg2rad(lat2-lat1);  // deg2rad below
+  const dLon = deg2rad(lon2-lon1); 
+  
+  const a = 
     Math.sin(dLat/2) * Math.sin(dLat/2) +
     Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
-    Math.sin(dLon/2) * Math.sin(dLon/2)
-    ; 
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-  var d = R * c; // Distance in km
+    Math.sin(dLon/2) * Math.sin(dLon/2); 
+
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  const d = R * c; // Distance in km
   return d;
 }
 
 function deg2rad(deg) {
-  return deg * (Math.PI/180)
+  return deg * (Math.PI/180);
 }
 
 exports.getServices = (lat, lng) => 
@@ -25,8 +26,6 @@ exports.getServices = (lat, lng) =>
 	new Promise((resolve,reject) => {
 
 		let services =[];
-
-		console.log(getDistanceFromLatLonInKm(lat, lng, -23.000626, -43.335466));
 
 		const cypher = "MATCH (p:Service) "
 					+"RETURN p ";
@@ -40,7 +39,9 @@ exports.getServices = (lat, lng) =>
 		    else{
 		    	results.forEach(function (obj) {
 		            let p = obj['p'];
-		            services.push(p);
+
+		            if(getDistanceFromLatLonInKm(lat, lng, p.lat, p.lng) < 1.5)
+		            	services.push(p);
 		        });
 
 				resolve({ status: 201, services: services });
