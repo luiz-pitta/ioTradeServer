66b8048e99df244cfcbbba047d90799eaf992ef1
Luiz Guilherme Pitta <empreendedores.2016@gmail.com>
2017-04-26 14:58:35 -0300

    t

---
+++ functions/sensor_price.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const db = require('../models/Connection');
+
+exports.getSensorPriceInformation = () => 
+	
+	new Promise((resolve,reject) => {
+
+		let sensorPrice =[];
+
+		const cypher = "MATCH (s:Sensor)-[r:IS_IN]->(g:Group) "
+					+"RETURN s, r.price, g.title";
+
+		db.cypher({
+		    query: cypher,
+		    lean: true
+		}, (err, results) =>{
+			if (err) 
+		    	reject({ status: 500, message: 'Internal Server Error !' });
+		    else{
+		    	results.forEach(function (obj) {
+		            let s = obj['s'];
+		            const price_sensor = obj['r.price'];
+		            const title_group = obj['g.title'];
+
+		            s.category = title_group;
+		            s.price = price_sensor;
+
+		            sensorPrice.push(s);
+		        });
+
+				resolve({ status: 201, sensorPrice: sensorPrice });
+		    }
+		    
+		});
+
+	});
