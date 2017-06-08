396648ab9f4dd9d4dad6eaa19030fa027ffd9869
Luiz Guilherme Pitta <empreendedores.2016@gmail.com>
2017-06-08 12:46:59 -0300

    T

--- functions/sensor.js
+++ functions/sensor.js
@@ -57,272 +57,333 @@ function randomIntFromInterval(min,max)
 /**
  * @return Retorna os serviços sem analytics do algoritmo de matchmaking.
  */
-exports.getSensorAlgorithm = (lat, lng, category) =>
+exports.getSensorAlgorithm = (lat, lng, category) => 
+	
+	new Promise((resolve,reject) => {
 
-    new Promise((resolve, reject) => {
+		let sensors =[];
+		let connect_chosen;
+		let sensor_chosen;
+		let high_rank = -1.0;
 
-        let sensors = [];
-        let connect_chosen;
-        let sensor_chosen;
-        let high_rank = -1.0;
+		const cypher = "MATCH (you:Profile) "
+					+"MATCH (cn:Conection)-[:IS_NEAR]->(s:Sensor)-[:BELONGS_TO]->(c:Category {title: {category}}) "
+					+"MATCH (s)-[sr:IS_IN]->(g:Group) MATCH (cn)-[cnr:IS_IN]->(g2:Group) "
+					+"WHERE (sr.price + cnr.price) <= you.budget "
+					+"RETURN cn, s, sr, cnr, g.title, g2.title ORDER BY cn.title";
 
-        const cypher = "MATCH (you:Profile) " +
-            "MATCH (cn:Conection)-[:IS_NEAR]->(s:Sensor)-[:BELONGS_TO]->(c:Category {title: {category}}) " +
-            "MATCH (s)-[sr:IS_IN]->(g:Group) MATCH (cn)-[cnr:IS_IN]->(g2:Group) " +
-            "WHERE (sr.price + cnr.price) <= you.budget " +
-            "RETURN cn, s, sr, cnr, g.title, g2.title ORDER BY cn.title";
+		try{
+			assert.isDefined(lat, 'Variável Existe!');
+		}catch(err){
+			console.log(err.message);
+		}
+		
+		try{
+			assert.isDefined(lng, 'Variável Existe!');
+		}catch(err){
+			console.log(err.message);
+		}
 
+		try{
+			assert.isDefined(category, 'Variável Existe!');
+		}catch(err){
+			console.log(err.message);
+		}
+						
 
-        db.cypher({
-            query: cypher
-            , params: {
-                category: category
-            }
-            , lean: true
-        }, (err, results) => {
+		db.cypher({
+		    query: cypher,
+		    params: {
+	            category: category											
+		    },
+		    lean: true
+		}, (err, results) =>{
+			try{
+				assert.notExists(err, 'Sem erro!');
+			}catch(err){
+				console.log(err.message);
+			}
 
-            if (err)
-                reject({
-                    status: 500
-                    , message: 'Internal Server Error !'
-                });
-            else {
-                let i, j;
+			if (err) 
+		    	reject({ status: 500, message: 'Internal Server Error !' });
+		    else{
+		    	let i, j;
 
-                if (results && results.length > 0) {
+		    	try{
+					assert.isDefined(results, 'Vetor Existe!');
+				}catch(err){
+					console.log(err.message);
+				}
 
-                    for (i = 0; i < results.length; i++) {
-                        let j = i;
-                        let obj = results[i];
-                        let obj_next = results[j];
-                        let cn = obj['cn'];
-                        let cnr = obj['cnr'];
+		    	if(results && results.length > 0){
 
-                        cn.rank = parseFloat(cnr.sum) / parseFloat(cnr.qty);
-                        cn.price = cnr.price;
-                        cn.category = obj['g2.title'];
+			    	for(i=0;i<results.length;i++){
+			    		let j = i;
+			    		let obj = results[i];
+			    		let obj_next = results[j];
+			    		let cn = obj['cn'];
+			    		let cnr = obj['cnr'];
 
-                        let cn_next = obj_next['cn'];
+			    		cn.rank = parseFloat(cnr.sum)/parseFloat(cnr.qty);
+			            cn.price = cnr.price;
+			            cn.category = obj['g2.title'];
 
-                        cn.array = [];
+			    		let cn_next = obj_next['cn'];
 
-                        while (cn.title == cn_next.title) {
-                            let s = obj_next['s'];
-                            let sr = obj_next['sr'];
-                            s.rank = parseFloat(sr.sum) / parseFloat(sr.qty);
-                            s.price = sr.price;
-                            s.category = obj_next['g.title'];
-                            cn.array.push(s);
+			    		cn.array = [];
+		
+			    		while(cn.title == cn_next.title){
+			    			let s = obj_next['s'];
+			    			let sr = obj_next['sr'];
+			    			s.rank = parseFloat(sr.sum)/parseFloat(sr.qty);
+			            	s.price = sr.price;
+			            	s.category = obj_next['g.title'];
+			            	cn.array.push(s);
 
-                            j++;
-                            if (j < results.length) {
-                                obj_next = results[j];
-                                cn_next = obj_next['cn'];
-                            } else {
-                                cn_next = String(-2);
-                            }
-                        }
-                        i = j - 1;
+							j++;
+							if(j < results.length){
+								obj_next = results[j];
+								cn_next = obj_next['cn'];
+							}
+							else{
+								cn_next = String(-2);
+							}
+						}
+						i=j-1;
 
-                        if (getDistanceFromLatLonInKm(lat, lng, cn.lat, cn.lng) < 1.5) {
-                            cn.array.sort(function (a, b) {
-                                if (a.rank < b.rank)
-                                    return 1;
-                                else if (a.rank > b.rank)
-                                    return -1;
-                                else if (a.price < b.price)
-                                    return -1;
-                                else if (a.price > b.price)
-                                    return 1;
-                                else
-                                    return 0;
-                            });
-                            sensors.push(cn);
-                        }
-                    }
+						if(getDistanceFromLatLonInKm(lat, lng, cn.lat, cn.lng) < 1.5){
+							cn.array.sort(function(a,b) {  
+							    if (a.rank < b.rank)
+				                    return 1;
+				                else if (a.rank > b.rank)
+				                    return -1;
+				                else if (a.price < b.price)
+				                    return -1;
+				                else if (a.price > b.price)
+				                	return 1;
+				                else
+				                	return 0;
+							});
+			            	sensors.push(cn);
+						}
+			    	}
 
-                    sensors.sort(function (a, b) {
-                        if (a.rank < b.rank)
-                            return 1;
-                        else if (a.rank > b.rank)
-                            return -1;
-                        else if (a.sgnl_net < b.sgnl_net)
-                            return 1;
-                        else if (a.sgnl_net > b.sgnl_net)
-                            return -1;
-                        else if (a.batery < b.batery)
-                            return 1;
-                        else if (a.batery > b.batery)
-                            return -1;
-                        else if (a.price < b.price)
-                            return -1;
-                        else if (a.price > b.price)
-                            return 1;
-                        else
-                            return 0;
-                    });
+			    	sensors.sort(function(a,b) {  
+					    if (a.rank < b.rank)
+		                    return 1;
+		                else if (a.rank > b.rank)
+		                    return -1;
+		                else if (a.sgnl_net < b.sgnl_net)
+		                    return 1;
+		                else if (a.sgnl_net > b.sgnl_net)
+		                    return -1;
+		                else if (a.batery < b.batery)
+		                    return 1;
+		                else if (a.batery > b.batery)
+		                	return -1;
+		                else if (a.price < b.price)
+		                    return -1;
+		                else if (a.price > b.price)
+		                	return 1;
+		                else
+		                	return 0;
+					});
 
-                    if (sensors.length > 0) {
-                        connect_chosen = sensors[0];
-                        let sensors_final = [];
-                        const high_rank = connect_chosen.array[0].rank;
+			    	if(sensors.length > 0){
+						connect_chosen = sensors[0];
+						let sensors_final = [];
+						const high_rank = connect_chosen.array[0].rank;
 
-                        connect_chosen.array.forEach(function (obj) {
-                            if (obj.rank == high_rank)
-                                sensors_final.push(obj);
-                        });
+						connect_chosen.array.forEach(function (obj) {
+				            if(obj.rank == high_rank)
+				            	sensors_final.push(obj);
+				        });
 
-                        if (sensors_final.length > 1) {
-                            const position = randomIntFromInterval(0, (sensors_final.length - 1));
-                            sensor_chosen = sensors_final[position];
-                        } else
-                            sensor_chosen = sensors_final[0];
-                    }
+				        if(sensors_final.length > 1){
+				        	const position = randomIntFromInterval(0, (sensors_final.length-1));
+				        	sensor_chosen = sensors_final[position];
+			       		}else
+			        		sensor_chosen = sensors_final[0];
+			    	}
 
-                    resolve({
-                        status: 201
-                        , sensor: sensor_chosen
-                        , connect: connect_chosen
-                    });
-                } else
-                    resolve({
-                        status: 201
-                        , sensor: null
-                        , connect: null
-                    });
+			    	try{
+						assert.exists(sensor_chosen, 'Variável Existe!');
+					}catch(err){
+						console.log(err.message);
+					}
 
-            }
+					try{
+						assert.exists(connect_chosen, 'Variável Existe!');
+					}catch(err){
+						console.log(err.message);
+					}
 
-        });
+					resolve({ status: 201, sensor: sensor_chosen, connect: connect_chosen });
+				}else
+					resolve({ status: 201, sensor: null, connect: null });
+				
+		    }
+		    
+		});
 
-    });
+	});
 
 /**
  * @return Retorna os serviços com analytics do algoritmo de matchmaking.
  */
-exports.getSensorAlgorithmAnalytics = (lat, lng, category) =>
+exports.getSensorAlgorithmAnalytics = (lat, lng, category) => 
+	
+	new Promise((resolve,reject) => {
 
-    new Promise((resolve, reject) => {
+		let sensors =[];
+		let chosen_group;
 
-        let sensors = [];
-        let chosen_group;
+		const cypher = "MATCH (you:Profile) "
+					+"MATCH (cn:Conection)-[:IS_NEAR]->(s:Sensor)-[:BELONGS_TO]->(c:Category {title: {category}}) "
+					+"MATCH (a:Analytics)-[:BELONGS_TO]->(c:Category {title: {category}}) "
+					+"MATCH (s)-[sr:IS_IN]->(g:Group)  "
+					+"MATCH (cn)-[cnr:IS_IN]->(g2:Group) "
+					+"MATCH (a)-[ar:IS_IN]->(g3:Group) "
+					+"WHERE (sr.price + cnr.price + ar.price) <= you.budget "
+					+"RETURN cn, cnr, s, sr, a ,ar, g.title, g2.title, g3.title ORDER BY cn.title, s.title, a.title";
 
-        const cypher = "MATCH (you:Profile) " +
-            "MATCH (cn:Conection)-[:IS_NEAR]->(s:Sensor)-[:BELONGS_TO]->(c:Category {title: {category}}) " +
-            "MATCH (a:Analytics)-[:BELONGS_TO]->(c:Category {title: {category}}) " +
-            "MATCH (s)-[sr:IS_IN]->(g:Group)  " +
-            "MATCH (cn)-[cnr:IS_IN]->(g2:Group) " +
-            "MATCH (a)-[ar:IS_IN]->(g3:Group) " +
-            "WHERE (sr.price + cnr.price + ar.price) <= you.budget " +
-            "RETURN cn, cnr, s, sr, a ,ar, g.title, g2.title, g3.title ORDER BY cn.title, s.title, a.title";
+		try{
+			assert.isDefined(lat, 'Variável Existe!');
+		}catch(err){
+			console.log(err.message);
+		}
+		
+		try{
+			assert.isDefined(lng, 'Variável Existe!');
+		}catch(err){
+			console.log(err.message);
+		}
 
-        db.cypher({
-            query: cypher
-            , params: {
-                category: category
-            }
-            , lean: true
-        }, (err, results) => {
+		try{
+			assert.isDefined(category, 'Variável Existe!');
+		}catch(err){
+			console.log(err.message);
+		}			
 
-            try {
-                assert.notExists(err, 'Sem erro!');
-            } catch (err) {
-                console.log(err.message);
-            }
+		db.cypher({
+		    query: cypher,
+		    params: {
+	            category: category											
+		    },
+		    lean: true
+		}, (err, results) =>{
 
-            if (err)
-                reject({
-                    status: 500
-                    , message: 'Internal Server Error !'
-                });
-            else {
-                let i, j;
+			try{
+				assert.notExists(err, 'Sem erro!');
+			}catch(err){
+				console.log(err.message);
+			}
+			
+			if (err) 
+		    	reject({ status: 500, message: 'Internal Server Error !' });
+		    else{
+		    	let i, j;
 
-                if (results && results.length > 0) {
+		    	try{
+					assert.isDefined(results, 'Vetor Existe!');
+				}catch(err){
+					console.log(err.message);
+				}
 
-                    results.forEach(function (obj) {
-                        let cn = obj['cn'];
-                        let cnr = obj['cnr'];
-                        let s = obj['s'];
-                        let sr = obj['sr'];
-                        let a = obj['a'];
-                        let ar = obj['ar'];
+		    	if(results && results.length > 0){
 
-                        cn.rank = parseFloat(cnr.sum) / parseFloat(cnr.qty);
-                        cn.price = cnr.price;
-                        cn.category = obj['g2.title'];
-                        s.rank = parseFloat(sr.sum) / parseFloat(sr.qty);
-                        s.price = sr.price;
-                        s.category = obj['g.title'];
-                        a.rank = parseFloat(ar.sum) / parseFloat(ar.qty);
-                        a.price = ar.price;
-                        a.category = obj['g3.title'];
+			    	results.forEach(function (obj) {
+			            let cn = obj['cn'];
+			            let cnr = obj['cnr'];
+			            let s = obj['s'];
+			            let sr = obj['sr'];
+			            let a = obj['a'];
+			            let ar = obj['ar'];
 
-                        cn.sensor = s;
-                        cn.analytics = a;
+			            cn.rank = parseFloat(cnr.sum)/parseFloat(cnr.qty);
+			            cn.price = cnr.price;
+			            cn.category = obj['g2.title'];
+			            s.rank = parseFloat(sr.sum)/parseFloat(sr.qty);
+			            s.price = sr.price;
+			            s.category = obj['g.title'];
+			            a.rank = parseFloat(ar.sum)/parseFloat(ar.qty);
+			            a.price = ar.price;
+			            a.category = obj['g3.title'];
 
-                        if (getDistanceFromLatLonInKm(lat, lng, cn.lat, cn.lng) < 1.5)
-                            sensors.push(cn);
+			            cn.sensor = s;
+			            cn.analytics = a;
 
-                    });
+			            if(getDistanceFromLatLonInKm(lat, lng, cn.lat, cn.lng) < 1.5)
+			            	sensors.push(cn);
+			            
+			        });
 
-                    sensors.sort(function (a, b) {
-                        if (a.rank < b.rank)
-                            return 1;
-                        else if (a.rank > b.rank)
-                            return -1;
-                        else if (a.sgnl_net < b.sgnl_net)
-                            return 1;
-                        else if (a.sgnl_net > b.sgnl_net)
-                            return -1;
-                        else if (a.batery < b.batery)
-                            return 1;
-                        else if (a.batery > b.batery)
-                            return -1;
-                        else if (a.price < b.price)
-                            return -1;
-                        else if (a.price > b.price)
-                            return 1;
-                      
-                        else if (a.sensor.rank < b.sensor.rank)
-                            return 1;
-                        else if (a.sensor.rank > b.sensor.rank)
-                            return -1;
-                        else if (a.sensor.price < b.sensor.price)
-                            return -1;
-                        else if (a.sensor.price > b.sensor.price)
-                            return 1;
+			    	sensors.sort(function(a,b) {  
+					    if (a.rank < b.rank)
+		                    return 1;
+		                else if (a.rank > b.rank)
+		                    return -1;
+		                else if (a.sgnl_net < b.sgnl_net)
+		                    return 1;
+		                else if (a.sgnl_net > b.sgnl_net)
+		                    return -1;
+		                else if (a.batery < b.batery)
+		                    return 1;
+		                else if (a.batery > b.batery)
+		                	return -1;
+		                else if (a.price < b.price)
+		                    return -1;
+		                else if (a.price > b.price)
+		                	return 1;
 
-                        else if (a.analytics.rank < b.analytics.rank)
-                            return 1;
-                        else if (a.analytics.rank > b.analytics.rank)
-                            return -1;
-                        else if (a.analytics.price < b.analytics.price)
-                            return -1;
-                        else if (a.analytics.price > b.analytics.price)
-                            return 1;
-                        else
-                            return 0;
-                    });
+		                else if (a.sensor.rank < b.sensor.rank)
+		                    return 1;
+		                else if (a.sensor.rank > b.sensor.rank)
+		                	return -1;
+		                else if (a.sensor.price < b.sensor.price)
+		                    return -1;
+		                else if (a.sensor.price > b.sensor.price)
+		                	return 1;
 
-                    chosen_group = sensors[0];
+		                else if (a.analytics.rank < b.analytics.rank)
+		                    return 1;
+		                else if (a.analytics.rank > b.analytics.rank)
+		                	return -1;
+		                else if (a.analytics.price < b.analytics.price)
+		                    return -1;
+		                else if (a.analytics.price > b.analytics.price)
+		                	return 1;
+		                else
+		                	return 0;
+					});
 
-                    resolve({
-                        status: 201
-                        , sensor: chosen_group.sensor
-                        , connect: chosen_group
-                        , analytics: chosen_group.analytics
-                    });
-                } else
-                    resolve({
-                        status: 201
-                        , sensor: null
-                        , connect: null
-                        , analytics: null
-                    });
+					chosen_group = sensors[0];
 
-            }
+					try{
+						assert.exists(chosen_group.sensor, 'Variável Existe!');
+					}catch(err){
+						console.log(err.message);
+					}
 
-        });
+					try{
+						assert.exists(chosen_group, 'Variável Existe!');
+					}catch(err){
+						console.log(err.message);
+					}
 
-    });
+					try{
+						assert.exists(chosen_group.analytics, 'Variável Existe!');
+					}catch(err){
+						console.log(err.message);
+					}
+
+
+					resolve({ status: 201, sensor: chosen_group.sensor, connect: chosen_group, analytics : chosen_group.analytics });
+				}else
+					resolve({ status: 201, sensor: null, connect: null, analytics : null });
+				
+		    }
+		    
+		});
+
+	});
--- routes.js
+++ routes.js
@@ -26,132 +26,140 @@ const get_sensor_matchmaking = require('./functions/sensor');
 
 const config = require('./config/config.json');
 const db = require('./models/Connection');
- 
+
 
 module.exports = router => {
 
-	router.get('/', (req, res) => res.end('IoTrade!'));
+    router.get('/', (req, res) => res.end('IoTrade!'));
 
-	/**
+    /**
      * @return Retorna uma mensagem que tudo ocorreu certo na atualização dos dados.
      */
-	router.post('/update_user_budget', (req, res) => {
+    router.post('/update_user_budget', (req, res) => {
 
-		const price = req.body.price;
+        const price = req.body.price;
 
-		update_user_budget.updateUserBudget(price)
+        update_user_budget.updateUserBudget(price)
 
-		.then(result => res.json({ message: result.message }))
+            .then(result => res.json({ message: result.message }))
 
-		.catch(err => res.status(err.status).json({ message: err.message }));
-		
-	});
+            .catch(err => res.status(err.status)
+                .json({ message: err.message }));
 
-	/**
+    });
+
+    /**
      * @return Retorna uma mensagem que tudo ocorreu certo na atualização dos dados.
      */
-	router.post('/update_sensor_information', (req, res) => {
+    router.post('/update_sensor_information', (req, res) => {
 
-		const price = req.body.price;
-		const title = req.body.title;
-		const category = req.body.category;
-		const category_new = req.body.category_new;
+        const price = req.body.price;
+        const title = req.body.title;
+        const category = req.body.category;
+        const category_new = req.body.category_new;
 
-		update_sensor_information.updateSensorInformation(title, price, category, category_new)
+        update_sensor_information.updateSensorInformation(title, price, category, category_new)
 
-		.then(result => res.json({ message: result.message }))
+            .then(result => res.json({ message: result.message }))
 
-		.catch(err => res.status(err.status).json({ message: err.message }));
-		
-	});
+            .catch(err => res.status(err.status)
+                .json({ message: err.message }));
 
-	/**
+    });
+
+    /**
      * @return Retorna uma mensagem que tudo ocorreu certo na atualização dos dados.
      */
-	router.post('/update_sensor_rating', (req, res) => {
+    router.post('/update_sensor_rating', (req, res) => {
 
-		const sensor = req.body.sensor;
-		const connect = req.body.connect;
-		const analytics = req.body.analytics;
+        const sensor = req.body.sensor;
+        const connect = req.body.connect;
+        const analytics = req.body.analytics;
 
-		update_sensor_information.updateSensorRating(sensor, connect, analytics)
+        update_sensor_information.updateSensorRating(sensor, connect, analytics)
 
-		.then(result => res.json({ message: result.message }))
+            .then(result => res.json({ message: result.message }))
 
-		.catch(err => res.status(err.status).json({ message: err.message }));
-		
-	});
+            .catch(err => res.status(err.status)
+                .json({ message: err.message }));
 
-	/**
+    });
+
+    /**
      * @return Retorna as informações dos sensores.
      */
-	router.get('/get_sensor_price_information', (req,res) => {
+    router.get('/get_sensor_price_information', (req, res) => {
 
-		sensor_price.getSensorPriceInformation()
+        sensor_price.getSensorPriceInformation()
 
-		.then(result => res.json({ sensorPriceArray: result.sensorPriceArray }))
+            .then(result => res.json({ sensorPriceArray: result.sensorPriceArray }))
 
-		.catch(err => res.status(err.status).json({ message: err.message }));
-	});
+            .catch(err => res.status(err.status)
+                .json({ message: err.message }));
+    });
 
-	/**
+    /**
      * @return Retorna as informações dos serviços.
      */
-	router.post('/get_services_information', (req,res) => {
+    router.post('/get_services_information', (req, res) => {
 
-		const lat = req.body.lat;
-		const lng = req.body.lng;
+        const lat = req.body.lat;
+        const lng = req.body.lng;
 
-		get_services.getServices(lat, lng)
+        get_services.getServices(lat, lng)
 
-		.then(result => res.json({ categories: result.categories }))
+            .then(result => res.json({ categories: result.categories }))
 
-		.catch(err => res.status(err.status).json({ message: err.message }));
-	});
+            .catch(err => res.status(err.status)
+                .json({ message: err.message }));
+    });
 
-	/**
+    /**
      * @return Retorna os serviços sem analytics do algoritmo de matchmaking.
      */
-	router.post('/get_sensor_matchmaking', (req,res) => {
+    router.post('/get_sensor_matchmaking', (req, res) => {
 
-		const lat = req.body.lat;
-		const lng = req.body.lng;
+        const lat = req.body.lat;
+        const lng = req.body.lng;
 
-		const category = req.body.service;
+        const category = req.body.service;
 
-		get_sensor_matchmaking.getSensorAlgorithm(lat, lng, category)
+        get_sensor_matchmaking.getSensorAlgorithm(lat, lng, category)
 
-		.then(result => res.json({ sensor: result.sensor, connect: result.connect }))
+            .then(result => res.json({ sensor: result.sensor, connect: result.connect }))
 
-		.catch(err => res.status(err.status).json({ message: err.message }));
-	});
+            .catch(err => res.status(err.status)
+                .json({ message: err.message }));
+    });
 
-	/**
+    /**
      * @return Retorna os serviços com analytics do algoritmo de matchmaking.
      */
-	router.post('/get_sensor_matchmaking_analytics', (req,res) => {
+    router.post('/get_sensor_matchmaking_analytics', (req, res) => {
 
-		const lat = req.body.lat;
-		const lng = req.body.lng;
+        const lat = req.body.lat;
+        const lng = req.body.lng;
 
-		const category = req.body.service;
+        const category = req.body.service;
 
-		get_sensor_matchmaking.getSensorAlgorithmAnalytics(lat, lng, category)
+        get_sensor_matchmaking.getSensorAlgorithmAnalytics(lat, lng, category)
 
-		.then(result => res.json({ sensor: result.sensor, connect: result.connect, analytics:result.analytics }))
+            .then(result => res.json({ sensor: result.sensor, connect: result.connect, analytics: result.analytics }))
 
-		.catch(err => res.status(err.status).json({ message: err.message }));
-	});
+            .catch(err => res.status(err.status)
+                .json({ message: err.message }));
+    });
 
-	/**
+    /**
      * @return Retorna o usuário do servidor.
      */
-	router.get('/get_user', (req,res) => {
+    router.get('/get_user', (req, res) => {
 
-		get_user.getProfile()
+        get_user.getProfile()
 
-		.then(result => res.json({ user: result.user }))
+            .then(result => res.json({ user: result.user }))
 
-		.catch(err => res.status(err.status).json({ message: err.message }));
-	});
+            .catch(err => res.status(err.status)
+                .json({ message: err.message }));
+    });
 }
